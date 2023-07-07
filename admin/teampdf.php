524dd45d014a87dc9225380688ce913504b2490b
Gray angel <mrabu0101@gmail.com>
2023-07-07 16:47:20 +0530

    retrived team details done showing team details

--- admin/teampdf.php
+++ admin/teampdf.php
@@ -15,11 +15,24 @@ require "../includes/dbconnect.php";
     $row = $res_team->fetch_assoc();
     $team_name = $row['team_name'];
     $captain_name = $row['captain_name'];
-    $captain_mobile = $row['captain_mobile'];
+    $captain_mobile = $row['mobile'];
     $team_id = $row['id'];
     $select_players = "SELECT * FROM players WHERE id = '$team_id'";
+    $res_player = $conn->query($select_players);
  }
 ?>
+<div class="pdf">
+    <div class="top">
+        <div class="top-left">
+            <h2>TEAME NAME : <?php echo $team_name?></h2>
+            <h2>CAPTAIN NAME : <?php echo $captain_name?></h2>
+            <h2>CAPTAIN MOBILE : <?php echo $captain_mobile?></h2>
+        </div>
+        <div class="top-right">
+            
+        </div>
+    </div>
+</div>
 </body>
 </html>
 
